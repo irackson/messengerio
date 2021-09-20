7207c52ad5224e1b2dd3215abccc7750a7dde3be
irackson <nasirus@protonmail.com>
2021-09-20 02:04:58 -0400

    init next

--- apps/next-frontend/jest.config.js
+++ apps/next-frontend/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  displayName: 'next-frontend',
-  preset: '../../jest.preset.js',
-  transform: {
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
-    '^.+\\.[tj]sx?$': 'babel-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/next-frontend',
+    displayName: 'next-frontend',
+    preset: '../../jest.preset.js',
+    transform: {
+        '^(?!.*\\.(js|jsx|ts|tsx|scss|json)$)': '@nrwl/react/plugins/jest',
+        '^.+\\.[tj]sx?$': 'babel-jest',
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    coverageDirectory: '../../coverage/apps/next-frontend',
 };
