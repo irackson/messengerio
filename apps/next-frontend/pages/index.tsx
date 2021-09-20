e561872494d2335161301344b9f8d0a0452716cc
irackson <nasirus@protonmail.com>
2021-09-20 13:26:45 -0400

    login id submit working

--- apps/next-frontend/pages/index.tsx
+++ apps/next-frontend/pages/index.tsx
@@ -1,9 +1,13 @@
 import styles from './index.module.scss';
 import { Login } from '@irackson/useful-comps';
+import { useState, useEffect } from 'react';
+
 export function Index() {
+    const [id, setId] = useState<string>();
+
     return (
         <div className={styles.page}>
-            <Login></Login>
+            <Login onIdSubmit={setId}></Login>
         </div>
     );
 }
--- libs/useful-comps/src/lib/login.tsx
+++ libs/useful-comps/src/lib/login.tsx
@@ -1,13 +1,44 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Dispatch, FormEvent, SetStateAction, useRef } from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import styled from 'styled-components';
 import './login.module.css';
 
 /* eslint-disable-next-line */
-export interface LoginProps {}
+export interface LoginProps {
+    onIdSubmit: Dispatch<SetStateAction<string>>;
+}
+
+const StyledLogin = styled.div`
+    /* background-color: gray; */
+`;
 
 export function Login(props: LoginProps) {
+    const idRef = useRef<HTMLInputElement>(null);
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        idRef.current?.value && props.onIdSubmit(idRef.current.value);
+    };
+
     return (
-        <div>
-            <h1>Welcome to Login!</h1>
-        </div>
+        <StyledLogin>
+            <Container
+                className="align-items-center d-flex"
+                style={{ height: '100vh' }}
+            >
+                <Form onSubmit={handleSubmit} className="w-100">
+                    <Form.Group>
+                        <Form.Label>Enter your Id</Form.Label>
+                        <Form.Control type="text" ref={idRef} required />
+                    </Form.Group>
+                    <Button type="submit" className="mr-2">
+                        Login
+                    </Button>
+                    <Button variant="secondary">Create A New Id</Button>
+                </Form>
+            </Container>
+        </StyledLogin>
     );
 }
 
