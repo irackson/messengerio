93762d2291c81ada355ec25b3e92b6f761b79a54
irackson <nasirus@protonmail.com>
2021-09-20 03:38:42 -0400

    storybook init

---
+++ libs/useful-comps/src/lib/UsefulComps.stories.tsx
@@ -0,0 +1,12 @@
+import { Story, Meta } from '@storybook/react';
+import { UsefulComps, UsefulCompsProps } from './UsefulComps';
+
+export default {
+    component: UsefulComps,
+    title: 'UsefulComps',
+} as Meta;
+
+const Template: Story<UsefulCompsProps> = (args) => <UsefulComps {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
